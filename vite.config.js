1cdce7ccae522d72d7a443dc79c5e444fd5f7280
Brett Poole <bpoole00@gmail.com>
2022-11-07 21:44:14 -0600

    added breeze package with vue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    assetsInclude: ['**/*.(svg|png|jpg)'],
+    build: {
+        assetsInlineLimit: 0,
+    },
+    server: {
+        host: '10.20.50.2'
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
